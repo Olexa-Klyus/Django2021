f470feabbfa48df2db858a683d4b529e0ca508ed
olexa <olexakljus@gmail.com>
2023-11-14 08:48:47 +0200

    les2

--- cars/views.py
+++ cars/views.py
@@ -12,12 +12,20 @@ from .serializers import CarSerializer
 # delete/destroy
 
 class CarListCreateView(APIView):
+    def get(self, *args, **kwargs):
+        cars = CarModel.objects.all()
+        serializer = CarSerializer(instance=cars, many=True)
+        return Response(serializer.data)
+
     def post(self, *args, **kwargs):
         data = self.request.data
-        # car = CarModel.objects.create(**data)
+        # car = CarModel.objects.create(**data) запис в базу тепер зробить серіалайзер під капотом своїм save()
+
         serializer = CarSerializer(data=data)
         if not serializer.is_valid():
             return Response(serializer.errors)
         serializer.save()
         print(serializer.data)
         return Response(serializer.data)
+
+# class CarUpdateRetriveDestroy(APIView):
