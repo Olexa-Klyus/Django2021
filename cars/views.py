7ea3be3942453a2bc3162a1be2c4209054891b97
olexa <olexakljus@gmail.com>
2023-11-14 19:09:49 +0200

    les2

--- cars/views.py
+++ cars/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 
 from .models import CarModel
 from .serializers import CarSerializer
+from rest_framework import status
 
 
 # CRUD
@@ -15,7 +16,7 @@ class CarListCreateView(APIView):
     def get(self, *args, **kwargs):
         cars = CarModel.objects.all()
         serializer = CarSerializer(instance=cars, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status.HTTP_200_OK)
 
     def post(self, *args, **kwargs):
         data = self.request.data
@@ -23,10 +24,10 @@ class CarListCreateView(APIView):
 
         serializer = CarSerializer(data=data)
         if not serializer.is_valid():
-            return Response(serializer.errors)
+            return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
         serializer.save()
         print(serializer.data)
-        return Response(serializer.data)
+        return Response(serializer.data, status.HTTP_201_CREATED)
 
 
 class CarUpdateRetriveDestroy(APIView):
@@ -34,8 +35,49 @@ class CarUpdateRetriveDestroy(APIView):
         car_id = kwargs.get('pk')
 
         if not CarModel.objects.filter(pk=car_id).exists():
-            return Response('Car with this id not found')
+            return Response('Car with this id not found', status.HTTP_404_NOT_FOUND)
 
         car = CarModel.objects.get(pk=car_id)
         serializer = CarSerializer(car)
-        return Response(serializer.data)
+        return Response(serializer.data, status.HTTP_200_OK)
+
+    def put(self, *args, **kwargs):
+        data = self.request.data
+
+        car_id = kwargs.get('pk')
+        if not CarModel.objects.filter(pk=car_id).exists():
+            return Response('Car with this id not found', status.HTTP_404_NOT_FOUND)
+
+        car = CarModel.objects.get(pk=car_id)
+        serializer = CarSerializer(car, data)
+        # if not serializer.is_valid():
+        #     return Response(serializer.errors)
+        # цю перевірку записуємо коротше
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status.HTTP_200_OK)
+
+    def patch(self, *args, **kwargs):
+        data = self.request.data
+
+        car_id = kwargs.get('pk')
+        if not CarModel.objects.filter(pk=car_id).exists():
+            return Response('Car with this id not found', status.HTTP_404_NOT_FOUND)
+
+        car = CarModel.objects.get(pk=car_id)
+        serializer = CarSerializer(car, data, partial=True)
+        # if not serializer.is_valid():
+        #     return Response(serializer.errors)
+        # цю перевірку записуємо коротше
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status.HTTP_200_OK)
+
+    def delete(self, *args, **kwargs):
+        pk = kwargs.get('pk')
+        if not CarModel.objects.filter(pk=pk).exists():
+            return Response('Car with this id not found', status.HTTP_404_NOT_FOUND)
+
+        car = CarModel.objects.get(pk=pk)
+        car.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- configs/urls.py
+++ configs/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from cars.views import CarListCreateView
+from cars.views import CarListCreateView, CarUpdateRetriveDestroy
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('cars', CarListCreateView.as_view())
+    path('cars', CarListCreateView.as_view()),
+    path('cars/<int:pk>', CarUpdateRetriveDestroy.as_view())
 ]
