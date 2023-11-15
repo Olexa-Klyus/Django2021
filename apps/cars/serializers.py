7ebfa59701403a2cf053d728ce44936f8c794944
olexa <olexakljus@gmail.com>
2023-11-15 07:28:13 +0200

    les2

--- apps/cars/serializers.py
+++ apps/cars/serializers.py
@@ -5,5 +5,5 @@ from .models import CarModel
 class CarSerializer(ModelSerializer):
     class Meta:
         model = CarModel
-        # fields = '__all__' можна просто всі поля або перерахувати
+        # fields = '__all__' можна просто всі поля включити або перерахувати
         fields = ('id', 'brand', 'price', 'year')
