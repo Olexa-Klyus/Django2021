bf93bb03657bca386a4de1f50a6478fb428b5d04
olexa <olexakljus@gmail.com>
2023-11-20 22:21:41 +0200

    les3

--- apps/cars/models.py
+++ apps/cars/models.py
@@ -1,14 +1,22 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class CarModel(models.Model):
     class Meta:
         db_table = 'cars'
-        verbose_name = 'Car' # для адмінки - представлення таблиці
+    # використання валідаторів
+    # brand = models.CharField(max_length=100)
+    # price = models.IntegerField(validators=(MaxValueValidator(100000), MinValueValidator(100)))
+    # year = models.IntegerField()
+    # create_at = models.DateTimeField(auto_now_add=True)
+    # update_at = models.DateTimeField(auto_now=True)
 
     brand = models.CharField(max_length=100)
     price = models.IntegerField()
     year = models.IntegerField()
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.brand
--- apps/cars/views.py
+++ apps/cars/views.py
@@ -1,4 +1,7 @@
+from django.db.models import Q
 from rest_framework.views import APIView
+from rest_framework.generics import GenericAPIView
+
 from rest_framework.response import Response
 
 from .models import CarModel
@@ -15,6 +18,30 @@ from rest_framework import status
 class CarListCreateView(APIView):
     def get(self, *args, **kwargs):
         cars = CarModel.objects.all()
+        # print(cars.query)
+
+        # використання фільтрів
+        # cars = cars.filter(brand='bmw', price=6000)
+        # cars = cars.filter(brand__in=('bmw', 'audi'))
+        # cars = cars.filter(brand__icontains='b')
+        # cars = cars.filter(price__range=(3000, 8000))
+
+        # можна імпортнути і використовувати Q
+        # cars = cars.filter(Q(price=4000) | Q(price=6000) | Q(brand='bmw'))
+        # cars = cars.filter(Q(price=4000) | Q(price=6000), brand='bmw')
+
+        # може бути фільтр від фільтра або сорт. Якщо спадання - знак мінус
+        # cars = cars.filter(price__gt=3000).order_by('-price', 'year')[:2]
+        # поки запит не виконався, а тільки сформувався, можна роздрукувати і cars.query cars.count
+        # коли запит виконався в базі, цих властивостей вже нема
+        # print(cars.count())
+        # print(cars.query)
+
+        price_gt = self.request.query_params.get('price_gt')
+        if price_gt:
+            cars = cars.filter(price__gt=price_gt)
+        print(cars.query)
+
         serializer = CarSerializer(instance=cars, many=True)
         return Response(serializer.data, status.HTTP_200_OK)
 
