d7d7c341f456035d3d7a62532301619a3e08b30e
olexa <olexakljus@gmail.com>
2023-11-22 18:48:49 +0200

    les3

--- apps/auto_parks/views.py
+++ apps/auto_parks/views.py
@@ -9,11 +9,12 @@ class AutoParksListCreateView(ListCreateAPIView):
     queryset = AutoParksModel.objects.all()
     serializer_class = AutoParkSerializer
 
-
+# створити нове авто через автопарк
 class AutoParkAddCarView(CreateAPIView):
     queryset = AutoParksModel.objects.all()
     serializer_class = CarSerializer
 
+    # щоб додати номер автопарку
     def perform_create(self, serializer):
         auto_park = self.get_object()
         serializer.save(auto_park=auto_park)
