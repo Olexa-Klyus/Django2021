c8cdc90692c88d733662d0fac94ea47ac4283e6a
olexa <olexakljus@gmail.com>
2023-12-07 21:15:39 +0200

    les 4 user

--- apps/users/models.py
+++ apps/users/models.py
@@ -7,8 +7,11 @@ from .managers import UserManager
 class UserModel(AbstractBaseUser, PermissionsMixin):
     class Meta:
         db_table = 'auth_user'
+        ordering = ['id']
 
     email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    is_superuser = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     create_at = models.DateTimeField(auto_now_add=True)
