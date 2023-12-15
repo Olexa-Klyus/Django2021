9b6a5719e556de4aab5c7dca2b00056d8ff2aefa
olexa <olexakljus@gmail.com>
2023-12-15 19:27:43 +0200

    les7

--- core/services/jwt_service.py
+++ core/services/jwt_service.py
@@ -1,3 +1,5 @@
+from typing import Type, Union
+
 from django.contrib.auth import get_user_model
 
 from rest_framework.generics import get_object_or_404
@@ -23,13 +25,13 @@ class RecoveryToken(ActivateToken):
 
 class JwtService:
     @staticmethod
-    def create_token(user):
-        return ActivateToken.for_user(user)
+    def create_token(user, token_class: Type[Union[Token, BlacklistMixin]]):
+        return token_class.for_user(user)
 
     @staticmethod
-    def validate_token(token):
+    def validate_token(token, token_class: Type[Union[Token, BlacklistMixin]]):
         try:
-            action_token = ActivateToken(token)
+            action_token = token_class(token)
             action_token.check_blacklist()
         except Exception:
             raise JwtException
