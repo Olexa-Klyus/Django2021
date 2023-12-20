8c4eb2d2941deb19c3fb70921ee3b91fb510558d
olexa <olexakljus@gmail.com>
2023-12-20 18:33:40 +0200

    swagger

--- configs/urls.py
+++ configs/urls.py
@@ -10,25 +10,25 @@ from rest_framework.permissions import AllowAny
 # from django.contrib import admin
 
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Autoparks API",
-        default_version='v1',
-        description="About cars",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=(AllowAny,),
-)
+# schema_view = get_schema_view(
+#     openapi.Info(
+#         title="Autoparks API",
+#         default_version='v1',
+#         description="About cars",
+#         terms_of_service="https://www.google.com/policies/terms/",
+#         contact=openapi.Contact(email="contact@snippets.local"),
+#         license=openapi.License(name="BSD License"),
+#     ),
+#     public=True,
+#     permission_classes=(AllowAny,),
+# )
 
 urlpatterns = [
     path('auth', include('apps.auth.urls')),
     path('cars', include('apps.cars.urls')),
     path('auto_parks', include('apps.auto_parks.urls')),
     path('users', include('apps.users.urls')),
-    path('swagger', schema_view.with_ui('swagger', cache_timeout=0)),
+    # path('swagger', schema_view.with_ui('swagger', cache_timeout=0)),
 ]
 # для відхоплення 400 і 500 помилок і повідомлення в json форматі є наступне:
 # але працює тільки при DEBUG = False
