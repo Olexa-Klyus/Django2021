a93dfdabd2a7e0f6d56b2de30e3d44da09fd2ed4
olexa <olexakljus@gmail.com>
2023-12-20 19:12:48 +0200

    swagger

--- configs/settings.py
+++ configs/settings.py
@@ -31,7 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'drf_yasg2',
+    'drf_yasg',
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
     'corsheaders',
--- configs/urls.py
+++ configs/urls.py
@@ -2,33 +2,33 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import include, path
 
-from drf_yasg2 import openapi
-from drf_yasg2.views import get_schema_view
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
 
 from rest_framework.permissions import AllowAny
 
 # from django.contrib import admin
 
 
-# schema_view = get_schema_view(
-#     openapi.Info(
-#         title="Autoparks API",
-#         default_version='v1',
-#         description="About cars",
-#         terms_of_service="https://www.google.com/policies/terms/",
-#         contact=openapi.Contact(email="contact@snippets.local"),
-#         license=openapi.License(name="BSD License"),
-#     ),
-#     public=True,
-#     permission_classes=(AllowAny,),
-# )
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Autoparks API",
+        default_version='v1',
+        description="About cars",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[AllowAny],
+)
 
 urlpatterns = [
     path('auth', include('apps.auth.urls')),
     path('cars', include('apps.cars.urls')),
     path('auto_parks', include('apps.auto_parks.urls')),
     path('users', include('apps.users.urls')),
-    # path('swagger', schema_view.with_ui('swagger', cache_timeout=0)),
+    path('swagger', schema_view.with_ui('swagger', cache_timeout=0)),
 ]
 # для відхоплення 400 і 500 помилок і повідомлення в json форматі є наступне:
 # але працює тільки при DEBUG = False
