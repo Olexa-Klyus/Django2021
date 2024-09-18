f6b0201c7569e1d7df5b8813a0e796d8d49e3584
Olexa-Klyus <olexakljus@gmail.com>
2024-09-18 19:48:14 +0300

    + передавання через body і kwarks

--- configs/urls.py
+++ configs/urls.py
@@ -1,9 +1,12 @@
 # from django.contrib import admin
 from django.urls import path, include
 
-from first.views import FirstView
+from first.views import FirstView, SecondView, ThirdView
+
 urlpatterns = [
-    #path('admin/', admin.site.urls),
-    path('first', FirstView.as_view())
+    # path('admin/', admin.site.urls),
+    path('first', FirstView.as_view()),
+    path('second', SecondView.as_view()),
+    path('third/<str:name>/<int:age>', ThirdView.as_view()),
 
 ]
--- first/views.py
+++ first/views.py
@@ -2,7 +2,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 
-# request тут
+# request тут обовязковий параметр
 class FirstView(APIView):
     def get(self, request):
         return Response('Method Get')
@@ -18,3 +18,29 @@ class FirstView(APIView):
 
     def delete(self, request):
         return Response('Method Delete')
+
+
+# для витягування  параметрів користуємося self.request, а обовязковий параметр request
+# ховаємо в args, щоб не муляв очі, тому що він не типізований
+class SecondView(APIView):
+    def get(self, *args, **kwargs):
+        query_params = self.request.query_params.dict()
+        print(query_params)
+        return Response(query_params)
+
+    # передавання через body
+    def post(self, *args, **kwargs):
+        data = self.request.data
+        print(data)
+        return Response(data)
+
+
+# передавання через динамічну частину урли і іменовані параметри, які попадають  в kwargs
+class ThirdView(APIView):
+    def get(self, *args, **kwargs):
+        name = kwargs.get('name')
+        age = kwargs.get('age')
+        print(age)
+        print(name)
+        print(kwargs)
+        return Response(kwargs)
