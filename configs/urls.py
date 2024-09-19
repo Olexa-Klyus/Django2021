03c32910cb1991a1a7f52aded8a07a47f25d0585
Olexa-Klyus <olexakljus@gmail.com>
2024-09-19 08:07:02 +0300

    serialiser base

--- configs/urls.py
+++ configs/urls.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from apps.cars.views import CarListCreateView, CarUpdateRetriveDestroy
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('cars', include('apps.cars.urls'))
 
 ]
